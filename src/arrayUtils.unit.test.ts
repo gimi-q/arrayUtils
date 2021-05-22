8e2f687aaab1486600af7c268b1af26a793e8ffa
Vegim Qehaja <vegim.qehaja@mvfglobal.com>
2021-05-22 21:17:28 +0200

    Add more test cases to check flatten handles many types of nested array values

--- src/arrayUtils.unit.test.ts
+++ src/arrayUtils.unit.test.ts
@@ -1,9 +1,10 @@
 import { flatten } from './arrayUtils.js';
+import { expectedCases } from './arrayUtils.unit.fixtures.js';
 
 describe('Array utility functions', () => {
   describe('Flatten', () => {
-    it('should flatten an arbitrarily nested array of values', () => {
-      expect(flatten([ 1, [ 2, [ 3 ] ], 4 ])).toEqual([1,2,3,4]);
+    it.each(expectedCases)('should flatten an arbitrarily nested array of values', (input, output) => {
+      expect(flatten(input)).toEqual(output);
     });
 
     xit('should gracefully handle strange or invalid inputs', () => {});
