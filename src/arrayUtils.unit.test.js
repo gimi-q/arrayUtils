f8518449b1dc41a97b8684c93f6be90aa6e90d98
Vegim Qehaja <vegim.qehaja@mvfglobal.com>
2021-05-24 11:05:26 +0200

    Update test cases with examples and standardise naming

--- src/arrayUtils.unit.test.js
+++ src/arrayUtils.unit.test.js
@@ -3,20 +3,20 @@ import { expectedCases, edgeCases } from './arrayUtils.unit.fixtures.js';
 
 describe('Array utility functions', () => {
   describe('Flatten', () => {
-    it.each(expectedCases)('should flatten an arbitrarily nested array of values', (input, output) => {
+    it.each(expectedCases)('should flatten an arbitrarily nested array of values e.g %s', (input, output) => {
       expect(flatten(input)).toEqual(output);
     });
 
-    it.each(edgeCases)('should gracefully handle strange or invalid inputs', (input, output) => {
+    it.each(edgeCases)('should gracefully handle strange or invalid inputs e.g %s', (input, output) => {
       expect(flatten(input)).toEqual(output);
     });
 
     it('should handle long arrays while maintaining correct order e.g 500,000 items', () => {
-      const values = Array(5e5).fill(1).map(item => [
+      const items = Array(5e5).fill(1).map(item => [
         item, 
         [[[[[[[[[[[[[[[[[[[[[[[[[item, 9]]]]]]]]]]]]]]]]]]]]]]]]],
       ]);
-      const result = flatten(values);
+      const result = flatten(items);
       const pattern = [1,1,9,1,1,9];
       
       expect(result.slice(0, 6)).toEqual(pattern);
